00183d5a0b209b558bc40adbf4268aa35b0e1b6b
Anshuman Komawar <anshumankomawar@gmail.com>
2022-12-25 14:20:10 +0530

    Updated project name in package.json

--- app/required-session/page.tsx
+++ app/required-session/page.tsx
@@ -2,11 +2,8 @@ import 'server-only';
 
 import Logout from '../../components/logout';
 
-// do not cache this page
 export const revalidate = 0;
 
-// the user will be redirected to the landing page if they are not signed in
-// check middleware.tsx to see how this routing rule is set
 export default async function RequiredSession() {
   return <Logout />;
 }
